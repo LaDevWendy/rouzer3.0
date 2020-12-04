1d2ae28bdd33e9a46a42026e9dba0b95a3c398bd
Pepe Lamberti <vilterti@gmail.com>
2020-12-04 14:11:15 -0300

    1.3.2 version cambiada

--- FrontEndWeb/src/components/Navbar.svelte
+++ FrontEndWeb/src/components/Navbar.svelte
@@ -47,7 +47,7 @@
             <Ripple/>
         </span>
         <a href="/" style="font-family: euroFighter">
-            <h3>ROZED <span class="version"> La red nini (Version Chad 1.3.0)</span></h3>
+            <h3>ROZED <span class="version"> La red nini (Version Chad 1.3.2)</span></h3>
 
             <Ripple/>
         </a>
