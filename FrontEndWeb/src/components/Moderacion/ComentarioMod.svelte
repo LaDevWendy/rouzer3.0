6372104a76f510a0ae292534368454cf94313e29
LaDevWendy <pinklady12oz@protonmail.com>
2022-06-01 18:51:09 -0300

    ComentarioMod
    Click en Id de comentario lleva al comentario

--- FrontEndWeb/src/components/Moderacion/ComentarioMod.svelte
+++ FrontEndWeb/src/components/Moderacion/ComentarioMod.svelte
@@ -2,10 +2,20 @@
     import { ComentarioEstado } from "../../enums";
     import Comentario from "../Comentarios/Comentario.svelte";
     export let comentario;
+
+    function tagCliqueado(e) {
+        let newWindow = `/Hilo/${comentario.hiloId}#${comentario.id}`;
+        window.open(newWindow, "_blank");
+        e.preventDefault();
+    }
 </script>
 
 <div class:eliminado={comentario.estado == ComentarioEstado.eliminado}>
-    <Comentario hilo={{ id: comentario.hiloId }} {comentario} />
+    <Comentario
+        hilo={{ id: comentario.hiloId }}
+        {comentario}
+        on:tagear={tagCliqueado}
+    />
 </div>
 
 <style>
