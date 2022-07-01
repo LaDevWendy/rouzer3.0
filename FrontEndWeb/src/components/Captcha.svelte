458ad9d8a791ce46b5ce8bcf9e8cedb0e2d61f63
LaDevWendy <pinklady12oz@protonmail.com>
2022-06-30 22:48:06 -0300

    Refresh para el captcha

--- FrontEndWeb/src/components/Captcha.svelte
+++ FrontEndWeb/src/components/Captcha.svelte
@@ -1,6 +1,7 @@
 <script>
     import { onMount } from "svelte";
     import globalStore from "../globalStore";
+    import { Button } from "svelte-mui";
     export let token;
     export let visible = true;
 
@@ -18,24 +19,52 @@
         });
     }
 
+    function reload() {
+        window.hcaptcha.reset();
+    }
+
     window.onCaptcha = (e) => {
         token = e;
     };
 </script>
 
 {#if visible}
-    <div
-        class="h-captcha"
-        id="super-captcha"
-        style="display:flex"
-        data-callback="onCaptcha"
-        data-theme="dark"
-        data-sitekey={window.config.hCaptchaSiteKey}
-    />
+    <div class="captcha-container">
+        <div
+            class="h-captcha"
+            id="super-captcha"
+            style="display:flex"
+            data-callback="onCaptcha"
+            data-theme="dark"
+            data-sitekey={window.config.hCaptchaSiteKey}
+        />
+        <div class="refresh-container">
+            <Button title="Refrescar captcha" icon dense on:click={reload}
+                ><icon class="fe fe-refresh-cw" /></Button
+            >
+        </div>
+    </div>
 {/if}
 
 <style>
     .h-captcha > :global(*) {
         margin: auto;
+    }
+    .captcha-container {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        border-radius: 4px;
+    }
+    .refresh-container {
+        height: 74px;
+        background-color: #333333;
+        border-color: white;
+        border-style: solid;
+        border-width: 2px;
+        border-radius: 0 4px 4px 0;
+        border-left-width: 0;
+        display: flex;
+        align-items: center;
     }
 </style>
